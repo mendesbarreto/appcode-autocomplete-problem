22b6ba21a730f540a2c9c9c957a147986a26e7e0
Douglas Mendes <mendes-barreto@live.com>
2019-09-28 13:35:45 -0400

    add project to show jetbrains the autocomplete problem

---
+++ AppcodeAutoComplete/AppcodeAutoComplete/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AppcodeAutoComplete
+//
+//  Created by Douglas Mendes on 2019-09-28.
+//  Copyright © 2019 Douglas Mendes. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func showProblemAutoCompleteAppCode() {
+        
+    }
+}
+
